57db2ca5806e7893f196478a3d02ae701c597cfe
hovanco <hovancoquangtri@gmail.com>
2020-08-25 11:33:12 +0700

    first commit

---
+++ src/constants/ActionType.js
@@ -0,0 +1,8 @@
+export const addCart = "addCart";
+export const getProduct = "getProduct";
+export const increase = "increase";
+export const reduction = "reduction";
+export const removeProduct = "removeProduct";
+
+
+
---
+++ src/reducers/product.js
@@ -0,0 +1,87 @@
+import * as ActionType from "../constants/ActionType";
+var initialState = {
+  products: [
+    {
+      "_id": "1",
+      "title": "Nike Shoes 01",
+      "src": "https://hc.com.vn/i/ecommerce/media/GS.001443_FEATURE_49347.jpg",
+      "description": "dien thoai oppo",
+      "content": "alisosfvsav vsdvsvsvv eemsbsmbsb",
+      "price": 7657,
+      "colors": ["blue", "cyan", "crimson", "teal"],
+      "count": 1
+    },
+    {
+      "_id": "2",
+      "title": "Nike Shoes 02",
+      "src": "https://cf.shopee.vn/file/0f79921b7fb9a2bcc24f083fcfc69e41",
+      "description": "dien thoai oppo",
+      "content": "alisosfvsav vsdvsvsvv eemsbsmbsb",
+      "price": 7657,
+      "colors": ["red", "black", "crimson", "teal"],
+      "count": 1
+    },
+    {
+      "_id": "3",
+      "title": "Nike Shoes 03",
+      "src": "https://cdn.tgdd.vn/Products/Images/42/217936/samsung-galaxy-s20-plus-600x600-fix-600x600.jpg",
+      "description": "dien thoai oppo",
+      "content": "alisosfvsav vsdvsvsvv eemsbsmbsb",
+      "price": 7657,
+      "colors": ["pink", "black", "yellow", "teal"],
+      "count": 1
+    },
+
+    {
+      "_id": "4",
+      "title": "Nike Shoes 04",
+      "src": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRP33kM9M4tSutDX3gULBDj22tvp-dD5v4IkA&usqp=CAU",
+      "description": "dien thoai oppo",
+      "content": "alisosfvsav vsdvsvsvv eemsbsmbsb",
+      "price": 7657,
+      "colors": ["black", "cyan", "crimson", "teal"],
+      "count": 1
+    },
+    {
+      "_id": "5",
+      "title": "Nike Shoes 05",
+      "src": "https://didongviet.vn/pub/media/catalog/product//d/i/dien-thoai-oppo-a92_1_1.jpg",
+      "description": "dien thoai oppo",
+      "content": "alisosfvsav vsdvsvsvv eemsbsmbsb",
+      "price": 7657,
+      "colors": ["blue", "cyan", "crimson", "teal"],
+      "count": 1
+    },
+    {
+      "_id": "6",
+      "title": "Nike Shoes 06",
+      "src": "https://cdn.nguyenkimmall.com/images/detailed/648/10045480_dien-thoai-samsung-a31-xanh-1.jpg",
+      "description": "dien thoai oppo",
+      "content": "alisosfvsav vsdvsvsvv eemsbsmbsb",
+      "price": 7657,
+      "colors": ["red", "black", "crimson", "teal"],
+      "count": 1
+    },
+    {
+      "_id": "7",
+      "title": "Nike Shoes 07",
+      "src": "https://cdn1.viettelstore.vn/images/Product/ProductImage/medium/391720663.jpeg",
+      "description": "dien thoai oppo",
+      "content": "alisosfvsav vsdvsvsvv eemsbsmbsb",
+      "price": 7657,
+      "colors": ["pink", "black", "yellow", "teal"],
+      "count": 1
+    },
+    {
+      "_id": "8",
+      "title": "Nike Shoes 08",
+      "src": "https://didongviet.vn/pub/media/catalog/product//h/u/huawei-y7-pro-xanh-didongviet.jpg",
+      "description": "dien thoai oppo",
+      "content": "alisosfvsav vsdvsvsvv eemsbsmbsb",
+      "price": 7657,
+      "colors": ["black", "cyan", "crimson", "teal"],
+      "count": 1
+    }
+  ],
+}
+export default product;
